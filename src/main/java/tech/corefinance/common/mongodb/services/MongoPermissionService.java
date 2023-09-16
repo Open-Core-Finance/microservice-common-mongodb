e0303a388f0f7b56ed81892735e96f470afd5116
Trung Doan <trung_bao.d@kbtgvn.tech>
2023-09-16 17:57:36 +0700

    Fix permission service issue

--- src/main/java/tech/corefinance/common/mongodb/services/MongoPermissionService.java
+++ src/main/java/tech/corefinance/common/mongodb/services/MongoPermissionService.java
@@ -1,5 +1,6 @@
 package tech.corefinance.common.mongodb.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +9,8 @@ import tech.corefinance.common.mongodb.model.MongoInternalServiceConfig;
 import tech.corefinance.common.mongodb.model.MongoPermission;
 import tech.corefinance.common.mongodb.model.MongoResourceAction;
 import tech.corefinance.common.service.AbstractPermissionService;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -22,5 +25,17 @@ public class MongoPermissionService extends AbstractPermissionService<MongoPermi
     public @NotNull MongoResourceAction newResourceAction(String resourceType, String action, String url,
                                                           RequestMethod requestMethod) {
         return new MongoResourceAction(resourceType, action, url, requestMethod);
+    }
+
+    @Override
+    protected TypeReference<List<MongoPermission>> getPermissionJsonParseType() {
+        return new TypeReference<>() {
+        };
+    }
+
+    @Override
+    protected TypeReference<List<MongoInternalServiceConfig>> getApiConfigJsonParseType() {
+        return new TypeReference<>() {
+        };
     }
 }
