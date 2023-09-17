06155aece91a5a6e9d635eb56e52987162f2a3a6
Trung Doan <doanbaotrung@gmail.com>
2023-09-17 14:38:00 +0700

    Permission initialize test

--- src/test/java/tech/corefinance/common/mongodb/support/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.corefinance.common.mongodb.support.repository;
-
-import org.springframework.stereotype.Repository;
-import tech.corefinance.common.mongodb.support.model.RoleTest;
-import tech.corefinance.common.repository.CommonResourceRepository;
-
-@Repository
-public interface RoleRepository extends CommonResourceRepository<RoleTest, String> {
-}
--- src/test/java/tech/corefinance/common/mongodb/support/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.corefinance.common.mongodb.support.repository;
-
-import org.springframework.stereotype.Repository;
-import tech.corefinance.common.mongodb.support.model.UserTest;
-import tech.corefinance.common.repository.CommonResourceRepository;
-
-@Repository
-public interface UserRepository extends CommonResourceRepository<UserTest, String> {
-}
